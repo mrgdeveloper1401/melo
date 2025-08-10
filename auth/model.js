845a1ec17c9141b7559c7bf265188e6840fa308a
mohammad goodarzi <m.goodarzi606@gmail.com>
2025-08-10 22:42:43 +0330

    add package sequelize-cli

--- auth/model.js
+++ auth/model.js
@@ -1,13 +1,14 @@
-const { DataTypes, Sequelize } = require('sequelize');
+import DataTypes from 'sequelize';
+import Sequelize from "sequelize";
 const sequelize = required("../core/model.js")
 
 
 class User extends Model {}
-class Follow extends Model {}
-class Wallet extends Model {}
-class CartNumber extends Model {}
-class Profile extends Model {}
-class UserNotification extends Model {}
+// class Follow extends Model {}
+// class Wallet extends Model {}
+// class CartNumber extends Model {}
+// class Profile extends Model {}
+// class UserNotification extends Model {}
 
 
 User.init(
@@ -67,203 +68,203 @@ User.init(
     }
 );
 
-Follow.init(
-    {
-        id: {
-            type: DataTypes.BIGINT,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        from_user_id: {
-            type: DataTypes.BIGINT,
-            allowNull: false
-        },
-        to_user_id: {
-            type: DataTypes.BIGINT,
-            allowNull: false
-        }
-    },
-    {
-        sequelize,
-        modelName: "Follow",
-        tableName: "follows"
-    }
-);
+// Follow.init(
+//     {
+//         id: {
+//             type: DataTypes.BIGINT,
+//             primaryKey: true,
+//             autoIncrement: true
+//         },
+//         from_user_id: {
+//             type: DataTypes.BIGINT,
+//             allowNull: false
+//         },
+//         to_user_id: {
+//             type: DataTypes.BIGINT,
+//             allowNull: false
+//         }
+//     },
+//     {
+//         sequelize,
+//         modelName: "Follow",
+//         tableName: "follows"
+//     }
+// );
 
-Wallet.init(
-    {
-        id: {
-            type: DataTypes.BIGINT,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        amount: {
-            type: DataTypes.DECIMAL(15, 3),
-            defaultValue: 0
-        },
-        is_active: {
-            type: DataTypes.BIGINT,
-            defaultValue: true
-        }
-    },
-    {
-        sequelize,
-        modelName: "Wallet",
-        tableName: "wallets"
-    }
-);
+// Wallet.init(
+//     {
+//         id: {
+//             type: DataTypes.BIGINT,
+//             primaryKey: true,
+//             autoIncrement: true
+//         },
+//         amount: {
+//             type: DataTypes.DECIMAL(15, 3),
+//             defaultValue: 0
+//         },
+//         is_active: {
+//             type: DataTypes.BIGINT,
+//             defaultValue: true
+//         }
+//     },
+//     {
+//         sequelize,
+//         modelName: "Wallet",
+//         tableName: "wallets"
+//     }
+// );
 
-User.associate = (models) => {
-    User.belongsToMany(
-        models.User,
-        {
-            through: models.Follow,
-            as: "following",
-            foreignKey: "from_user_id",
-            otherKey: "to_user_id"
-        }
-    );
+// User.associate = (models) => {
+//     User.belongsToMany(
+//         models.User,
+//         {
+//             through: models.Follow,
+//             as: "following",
+//             foreignKey: "from_user_id",
+//             otherKey: "to_user_id"
+//         }
+//     );
 
-    User.belongsToMany(
-        models.User,
-        {
-            through: models.Follow,
-            as: 'followers',
-            foreignKey: 'to_user_id',
-            otherKey: 'from_user_id'
-        }
-    );
+//     User.belongsToMany(
+//         models.User,
+//         {
+//             through: models.Follow,
+//             as: 'followers',
+//             foreignKey: 'to_user_id',
+//             otherKey: 'from_user_id'
+//         }
+//     );
 
-    User.hasOne(
-        models.Wallet,
-        {
-            foreignKey: "user_id",
-            as: "wallet"
-        }
-    );
+//     User.hasOne(
+//         models.Wallet,
+//         {
+//             foreignKey: "user_id",
+//             as: "wallet"
+//         }
+//     );
 
-    User.hasOne(
-        models.Profile,
-        {
-            foreignKey: "user_id",
-            as: "profile"
-        }
-    );
-}
+//     User.hasOne(
+//         models.Profile,
+//         {
+//             foreignKey: "user_id",
+//             as: "profile"
+//         }
+//     );
+// }
 
-Wallet.associate = (models) => {
-    Wallet.belongsTo(
-        model.User,
-        {
-            foreignKey: "user_id",
-            as: 'user'
-        }
-    );
+// Wallet.associate = (models) => {
+//     Wallet.belongsTo(
+//         model.User,
+//         {
+//             foreignKey: "user_id",
+//             as: 'user'
+//         }
+//     );
 
-    Wallet.hasMany(
-        models.CartNumber,
-        {
-            foreignKey: 'wallet_id',
-            as: 'cart_numbers'
-        }
-    )
-}
+//     Wallet.hasMany(
+//         models.CartNumber,
+//         {
+//             foreignKey: 'wallet_id',
+//             as: 'cart_numbers'
+//         }
+//     )
+// }
 
-CartNumber.init(
-  {
-    id: {
-      type: DataTypes.BIGINT,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    cart: {
-      type: DataTypes.CHAR(16),
-      allowNull: false
-    },
-    is_active: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true
-    },
-    is_default: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    }
-  },
-  {
-    sequelize,
-    modelName: "CartNumber",
-    tableName: "cart_numbers",
-  }
-);
+// CartNumber.init(
+//   {
+//     id: {
+//       type: DataTypes.BIGINT,
+//       primaryKey: true,
+//       autoIncrement: true
+//     },
+//     cart: {
+//       type: DataTypes.CHAR(16),
+//       allowNull: false
+//     },
+//     is_active: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: true
+//     },
+//     is_default: {
+//       type: DataTypes.BOOLEAN,
+//       defaultValue: false
+//     }
+//   },
+//   {
+//     sequelize,
+//     modelName: "CartNumber",
+//     tableName: "cart_numbers",
+//   }
+// );
 
-CartNumber.associate = (models) => {
-  CartNumber.belongsTo(models.Wallet, {
-    foreignKey: 'wallet_id',
-    as: 'wallet'
-  });
-};
+// CartNumber.associate = (models) => {
+//   CartNumber.belongsTo(models.Wallet, {
+//     foreignKey: 'wallet_id',
+//     as: 'wallet'
+//   });
+// };
 
-Profile.init(
-  {
-    id: {
-      type: DataTypes.BIGINT,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    first_name: {
-      type: DataTypes.CHAR(255)
-    },
-    last_name: {
-      type: DataTypes.CHAR(255)
-    },
-    birth_date: {
-      type: DataTypes.DATE
-    },
-    bio: {
-      type: DataTypes.TEXT
-    },
-    jobs: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      defaultValue: []
-    },
-    social: {
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      defaultValue: []
-    }
-  },
-  {
-    sequelize,
-    modelName: "Profile",
-    tableName: "profiles",
-  }
-);
+// Profile.init(
+//   {
+//     id: {
+//       type: DataTypes.BIGINT,
+//       primaryKey: true,
+//       autoIncrement: true
+//     },
+//     first_name: {
+//       type: DataTypes.CHAR(255)
+//     },
+//     last_name: {
+//       type: DataTypes.CHAR(255)
+//     },
+//     birth_date: {
+//       type: DataTypes.DATE
+//     },
+//     bio: {
+//       type: DataTypes.TEXT
+//     },
+//     jobs: {
+//       type: DataTypes.ARRAY(DataTypes.STRING),
+//       defaultValue: []
+//     },
+//     social: {
+//       type: DataTypes.ARRAY(DataTypes.STRING),
+//       defaultValue: []
+//     }
+//   },
+//   {
+//     sequelize,
+//     modelName: "Profile",
+//     tableName: "profiles",
+//   }
+// );
 
-UserNotification.init(
-  {
-    id: {
-      type: DataTypes.BIGINT,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    title: {
-      type: DataTypes.CHAR(255),
-      allowNull: false
-    },
-    body: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    },
-    notification_redirect_url: {
-      type: DataTypes.CHAR(100)
-    },
-    notification_type: {
-      type: DataTypes.CHAR(30)
-    }
-  },
-  {
-    sequelize,
-    modelName: "UserNotification",
-    tableName: "user_notifications",
-    timestamps: false
-  }
-);
+// UserNotification.init(
+//   {
+//     id: {
+//       type: DataTypes.BIGINT,
+//       primaryKey: true,
+//       autoIncrement: true
+//     },
+//     title: {
+//       type: DataTypes.CHAR(255),
+//       allowNull: false
+//     },
+//     body: {
+//       type: DataTypes.TEXT,
+//       allowNull: false
+//     },
+//     notification_redirect_url: {
+//       type: DataTypes.CHAR(100)
+//     },
+//     notification_type: {
+//       type: DataTypes.CHAR(30)
+//     }
+//   },
+//   {
+//     sequelize,
+//     modelName: "UserNotification",
+//     tableName: "user_notifications",
+//     timestamps: false
+//   }
+// );
