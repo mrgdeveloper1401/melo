19a6d2ff25c60e015a55a7d4a4ae2de86b49fe48
mohammad goodarzi <m.goodarzi606@gmail.com>
2025-08-16 14:36:08 +0330

    add schema auth

--- src/models/auth/model.js
+++ src/models/auth/model.js
@@ -1,11 +1,4 @@
-import {DataTypes, Sequelize} from 'sequelize';
-
-// class User extends Model {}
-// class Follow extends Model {}
-// class Wallet extends Model {}
-// class CartNumber extends Model {}
-// class Profile extends Model {}
-// class UserNotification extends Model {}
+import DataTypes from 'sequelize';
 
 
 export const CreateUserModel = (sequelize) => {
@@ -73,203 +66,149 @@ export const CreateUserModel = (sequelize) => {
 }
 
 
-// Follow.init(
-//     {
-//         id: {
-//             type: DataTypes.BIGINT,
-//             primaryKey: true,
-//             autoIncrement: true
-//         },
-//         from_user_id: {
-//             type: DataTypes.BIGINT,
-//             allowNull: false
-//         },
-//         to_user_id: {
-//             type: DataTypes.BIGINT,
-//             allowNull: false
-//         }
-//     },
-//     {
-//         sequelize,
-//         modelName: "Follow",
-//         tableName: "follows"
-//     }
-// );
+export const followTable = (sequelize) => {
+    const follow = sequelize.define(
+        "follow",
+        {
+            id: {
+                type: DataTypes.BIGINT,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            from_user_id: {
+                type: DataTypes.BIGINT,
+                allowNull: false
+            },
+            to_user_id: {
+                type: DataTypes.BIGINT,
+                allowNull: false
+            }
+        },
+        {
+            timestamps: false
+        }
+    );
+    return follow;
+}
 
-// Wallet.init(
-//     {
-//         id: {
-//             type: DataTypes.BIGINT,
-//             primaryKey: true,
-//             autoIncrement: true
-//         },
-//         amount: {
-//             type: DataTypes.DECIMAL(15, 3),
-//             defaultValue: 0
-//         },
-//         is_active: {
-//             type: DataTypes.BIGINT,
-//             defaultValue: true
-//         }
-//     },
-//     {
-//         sequelize,
-//         modelName: "Wallet",
-//         tableName: "wallets"
-//     }
-// );
+export const WalletTable = (sequelize) => {
+    const wallet = sequelize.define(
+        "wallet",
+        {
+            id: {
+                type: DataTypes.BIGINT,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            amount: {
+                type: DataTypes.DECIMAL(15, 3),
+                defaultValue: 0
+            },
+            is_active: {
+                type: DataTypes.BIGINT,
+                defaultValue: true
+            }
+        },
+        {
+            timestamps: false
+        }
+    );
+};
 
-// User.associate = (models) => {
-//     User.belongsToMany(
-//         models.User,
-//         {
-//             through: models.Follow,
-//             as: "following",
-//             foreignKey: "from_user_id",
-//             otherKey: "to_user_id"
-//         }
-//     );
+export const CartNumberTable = (sequelize) => {
+    const cartNumber = sequelize.define(
+        "cart_number",
+    {
+        id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true
+        },
+        cart: {
+        type: DataTypes.CHAR(16),
+        allowNull: false
+        },
+        is_active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+        },
+        is_default: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+        }
+    },
+    {
+        timestamps: false
+    }
+    )
+};
 
-//     User.belongsToMany(
-//         models.User,
-//         {
-//             through: models.Follow,
-//             as: 'followers',
-//             foreignKey: 'to_user_id',
-//             otherKey: 'from_user_id'
-//         }
-//     );
+export const ProfileTable = (sequelize) => {
+    const profile = sequelize.define(
+        "profile",
+    {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+            first_name: {
+            type: DataTypes.CHAR(255)
+        },
+            last_name: {
+            type: DataTypes.CHAR(255)
+        },
+            birth_date: {
+            type: DataTypes.DATE
+        },
+            bio: {
+            type: DataTypes.TEXT
+        },
+            jobs: {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+            defaultValue: []
+        },
+            social: {
+            type: DataTypes.ARRAY(DataTypes.STRING),
+            defaultValue: []
+        }
+    },
+    {
+        timestamps: false
+    }
 
-//     User.hasOne(
-//         models.Wallet,
-//         {
-//             foreignKey: "user_id",
-//             as: "wallet"
-//         }
-//     );
+    )
+};
 
-//     User.hasOne(
-//         models.Profile,
-//         {
-//             foreignKey: "user_id",
-//             as: "profile"
-//         }
-//     );
-// }
-
-// Wallet.associate = (models) => {
-//     Wallet.belongsTo(
-//         model.User,
-//         {
-//             foreignKey: "user_id",
-//             as: 'user'
-//         }
-//     );
-
-//     Wallet.hasMany(
-//         models.CartNumber,
-//         {
-//             foreignKey: 'wallet_id',
-//             as: 'cart_numbers'
-//         }
-//     )
-// }
-
-// CartNumber.init(
-//   {
-//     id: {
-//       type: DataTypes.BIGINT,
-//       primaryKey: true,
-//       autoIncrement: true
-//     },
-//     cart: {
-//       type: DataTypes.CHAR(16),
-//       allowNull: false
-//     },
-//     is_active: {
-//       type: DataTypes.BOOLEAN,
-//       defaultValue: true
-//     },
-//     is_default: {
-//       type: DataTypes.BOOLEAN,
-//       defaultValue: false
-//     }
-//   },
-//   {
-//     sequelize,
-//     modelName: "CartNumber",
-//     tableName: "cart_numbers",
-//   }
-// );
-
-// CartNumber.associate = (models) => {
-//   CartNumber.belongsTo(models.Wallet, {
-//     foreignKey: 'wallet_id',
-//     as: 'wallet'
-//   });
-// };
-
-// Profile.init(
-//   {
-//     id: {
-//       type: DataTypes.BIGINT,
-//       primaryKey: true,
-//       autoIncrement: true
-//     },
-//     first_name: {
-//       type: DataTypes.CHAR(255)
-//     },
-//     last_name: {
-//       type: DataTypes.CHAR(255)
-//     },
-//     birth_date: {
-//       type: DataTypes.DATE
-//     },
-//     bio: {
-//       type: DataTypes.TEXT
-//     },
-//     jobs: {
-//       type: DataTypes.ARRAY(DataTypes.STRING),
-//       defaultValue: []
-//     },
-//     social: {
-//       type: DataTypes.ARRAY(DataTypes.STRING),
-//       defaultValue: []
-//     }
-//   },
-//   {
-//     sequelize,
-//     modelName: "Profile",
-//     tableName: "profiles",
-//   }
-// );
-
-// UserNotification.init(
-//   {
-//     id: {
-//       type: DataTypes.BIGINT,
-//       primaryKey: true,
-//       autoIncrement: true
-//     },
-//     title: {
-//       type: DataTypes.CHAR(255),
-//       allowNull: false
-//     },
-//     body: {
-//       type: DataTypes.TEXT,
-//       allowNull: false
-//     },
-//     notification_redirect_url: {
-//       type: DataTypes.CHAR(100)
-//     },
-//     notification_type: {
-//       type: DataTypes.CHAR(30)
-//     }
-//   },
-//   {
-//     sequelize,
-//     modelName: "UserNotification",
-//     tableName: "user_notifications",
-//     timestamps: false
-//   }
-// );
+export const UserNotificationTable = (sequelize) => {
+    const UserNotification = sequelize.define(
+        "user_notification",
+        {
+        id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.CHAR(255),
+            allowNull: false
+        },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        notification_redirect_url: {
+            type: DataTypes.CHAR(100),
+            allowNull: true
+        },
+        notification_type: {
+            type: DataTypes.CHAR(30),
+            allowNull: true
+        }
+    },
+    {
+        timestamps: false
+    }
+        
+    )
+};
